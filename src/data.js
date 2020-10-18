40bd593a597aba3f27f40f847640d783686f315d
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-10-18 23:56:26 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export default [
+    {id:1,name:'Sanam',age:29,image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',},
+    {id:2,name:'Rebecca',age:32,image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',},
+    {id:3,name:'Denisha',age:17, image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',},
+    {id:4,name:'Francis',age:23,image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',}
+];
